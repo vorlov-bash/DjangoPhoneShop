79e6e0b6471e3908f8a7cafa759ca0864ab12d8c
OXFORDX <volodka.orlov@gmail.com>
2019-12-14 21:04:18 +0200

    Install heroku and added requirements.txt

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +122,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
