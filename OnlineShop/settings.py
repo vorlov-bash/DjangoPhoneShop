821d2dff67c6b6cd8ecb7c5b09363b50f426242a
OXFORDX <volodka.orlov@gmail.com>
2019-12-15 03:17:15 +0200

    staticfiles

--- OnlineShop/settings.py
+++ OnlineShop/settings.py
@@ -120,6 +120,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
