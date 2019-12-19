0e021d25025572df7fc9cab8a14a993d5bc9a516
OXFORDX <volodka.orlov@gmail.com>
2019-12-19 19:33:08 +0200

    Deleted @font-face, added cart id session

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.item_list, name='main'),
-    path('<int:id>', views.product, name='product')
+    path('<int:id>', views.product, name='product'),
+    path('cart_add', views.cart_add, name='add'),
+    path('logout', views.logout, name='logout')
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Phone
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from .models import Phone, Cart
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -7,13 +10,42 @@ from .models import Phone
 def item_list(request):
     data = Phone.objects.all()
     print(data)
+    if not request.session.session_key:
+        request.session.save()
     return render(request, 'main_page.html', {'data': data})
-
-
-def test(request):
-    return render(request, 'test.html')
 
 
 def product(request, id):
     phone = Phone.objects.get(id=id)
     return render(request, 'product.html', {'phone': phone})
+
+
+@csrf_exempt
+def cart_add(request):
+    item = request.POST.get('name')
+    obj = cart_check(request.user.username) if request.user.is_authenticated else request.session.session_key
+    if item in obj.items:
+        obj.items[item] += 1
+    else:
+        obj.items[item] = 1
+    if request.user.is_authenticated:
+        request.session['cart_id'] = obj.id
+    obj.save()
+    return JsonResponse({'success': 1})
+
+
+@login_required
+def logout(request):
+    print(request.user.username)
+    logout(request)
+    return render(request, 'main_page.html')
+
+
+# Not views
+def cart_check(username):
+    try:
+        cart_obj = Cart.objects.get(user=username)
+        return cart_obj
+    except Cart.DoesNotExist:
+        cart_obj = Cart.objects.create(user=username, items={}, cost=0)
+        return cart_obj
